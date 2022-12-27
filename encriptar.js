899af0ea00489fefa62793f75fe1fb3612001790
gaburra-m <gaburra.m@gmail.com>
2022-12-27 14:21:05 -0600

    Se agregaron comprobaciones

--- encriptar.js
+++ encriptar.js
@@ -1,8 +1,8 @@
 let btnEncriptar = document.getElementById("encriptar")
-btnEncriptar.onclick = encriptar
+btnEncriptar.onclick = comprobar
 
 let btnDesencriptar = document.getElementById("desencriptar")
-btnDesencriptar.onclick = desencriptar
+btnDesencriptar.onclick = comprobarDesencriptar
 
 let btnCopiar = document.getElementById("copiar")
 btnCopiar.onclick = copiar
@@ -42,4 +42,52 @@ function borrarInput(){
         input.value = ""
     }
 }
+function comprobar(){
+    let textoInput = document.getElementById("input").value
+    let expreg = /[A-ZÁÉÍÓÚáéíóúÑ]+/g
 
+    if((textoInput === "")){
+        document.getElementById("error").setAttribute("style", "display: block;");
+        document.getElementById("vacio").setAttribute("style", "display: block;");
+        setTimeout(() => {
+            document.getElementById("error").setAttribute("style", "display: none;");
+            document.getElementById("vacio").setAttribute("style", "display: none;");
+            borrarInput()
+        }, 2000);
+    }else if(expreg.test(textoInput)){
+        document.getElementById("error").setAttribute("style", "display: block;");
+        document.getElementById("escribe").setAttribute("style", "display: block;");
+        setTimeout(() => {
+            document.getElementById("error").setAttribute("style", "display: none;");
+            document.getElementById("escribe").setAttribute("style", "display: none;")
+            borrarInput()
+        }, 2000);
+    }else {
+        encriptar()
+    }
+}
+function comprobarDesencriptar(){
+    let textoInput = document.getElementById("input").value
+    let expreg = /[A-ZÁÉÍÓÚáéíóúÑ]+/g
+
+    if((textoInput === "")){
+        document.getElementById("error").setAttribute("style", "display: block;");
+        document.getElementById("vacio").setAttribute("style", "display: block;");
+        setTimeout(() => {
+            document.getElementById("error").setAttribute("style", "display: none;");
+            document.getElementById("vacio").setAttribute("style", "display: none;");
+            borrarInput()
+        }, 2000);
+    }else if(expreg.test(textoInput)){
+        document.getElementById("error").setAttribute("style", "display: block;");
+        document.getElementById("escribe").setAttribute("style", "display: block;");
+        setTimeout(() => {
+            document.getElementById("error").setAttribute("style", "display: none;");
+            document.getElementById("escribe").setAttribute("style", "display: none;")
+            borrarInput()
+        }, 2000);
+    }else {
+        desencriptar()
+    }
+
+}
